6019d7a96208683bf285978dfd31ac2ce230a625
Tiago Inácio <tiago.inacio@blip.pt>
2018-06-18 07:00:42 +0100

    feat(bk): Change bookmark script to bk

--- fzf-bookmark.zsh
+++ fzf-bookmark.zsh
@@ -1,3 +1,7 @@
+bk_widget() LBUFFER+=$(bk)
+zle -N bk_widget
+bindkey '^b' bk_widget
+
 strip_duplicates() {
     # unique values only
     sort -u ~/.fzf_bookmarks > ~/.fzf_bookmarks_backup
@@ -5,22 +9,43 @@ strip_duplicates() {
     mv ~/.fzf_bookmarks_backup ~/.fzf_bookmarks
 }
 
-bookmark-rec() {
-    _dir=`find $1 -type d -maxdepth $2`;
-    for i in $_dir; do
-        echo $i >> ~/.fzf_bookmarks
-    done
-
-    strip_duplicates
+bk-rm() {
+    echo "$1 was removed from bookmarks."
+    sed -n '/^${1}$/!p' ~/.fzf_bookmarks
+    # mv ~/.fzf_bookmarks_backup ~/.fzf_bookmarks
 }
 
-bookmark-dir() {
-    echo $1 >> ~/.fzf_bookmarks
-
-    strip_duplicates
-}
-
-bookmark-list() {
-    local dir
-    dir=$(cat ~/.fzf_bookmarks | fzf +m) && cd "$dir"
+bk() {
+    if [ -z $1 ]; then
+        local dir
+        dir=$(cat ~/.fzf_bookmarks | fzf +m) && echo "$dir"
+    else
+        if [ -z $2 ]; then
+            if [ "${1}" = "." ]; then
+                echo `pwd` >> ~/.fzf_bookmarks
+                echo "`pwd` was bookmarked."
+            else
+                # remove trailling / if there is one
+                if [ "${1[-1]}" = "/" ]; then
+                    echo ${1[1,-2]} >> ~/.fzf_bookmarks
+                    echo "${1[1,-2]} was bookmarked."
+                else
+                    echo $1 >> ~/.fzf_bookmarks
+                    echo "${1} was bookmarked."
+                fi
+            fi
+        else
+            _dir=`find $1 -type d -maxdepth $2 | sed "s|^\./||"`;
+            for i in $_dir; do
+                if [ "${i[-1]}" = "/" ]; then
+                    echo ${i[1,-2]} >> ~/.fzf_bookmarks
+                    echo "${i[1,-2]} was bookmarked."
+                else
+                    echo $i >> ~/.fzf_bookmarks
+                    echo "${i} was bookmarked."
+                fi
+            done
+        fi
+        strip_duplicates
+    fi
 }
